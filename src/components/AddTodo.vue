a354d420e81dbd84265ef5b87e63ec185fad4f87
菅家俊太 <shunta@MacBook-Pro.local>
2020-06-05 20:44:44 +0900

    This is todolist commit & LocalStorage

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <form @submit="addTodo">
+            <input type="text" name="title" v-model="title" placeholder="add todo">
+            <input type="submit"  value="register" class="btn">
+        </form>
+    </div>
+</template>
+
+
+<script>
+import { v4 as uuidv4 } from 'uuid';
+
+export default {
+    name: "AddTodo",
+    data() {
+        return {
+            title:''
+        }
+    },
+    methods : {
+        addTodo(e) {
+            e.preventDefault();
+            const newTodo = {
+                id:uuidv4(),
+                title: this.title,
+                completed:false
+            }
+            confirm("don't you give up halfaway???")
+            this.$emit('add-todo',newTodo);
+
+            this.title ='';
+        }
+    }
+}
+</script>
+
+
+
+<style scoped>
+    form {
+        display:flex;
+
+    }
+
+    input[type="text"] {
+        flex: 10;
+        padding: 5px;
+    }
+
+    input[type="submit"] {
+        flex: 2;
+    }
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+// vue.config.js
+module.exports = {
+    publicPath: '/todo/',
+    outputDir: 'docs'
+}
